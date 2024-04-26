ca244d830d64a3d1a67d270716cdea7276f4a115
Marcel Herr <mrclhrr@gmail.com>
2024-04-26 12:09:45 +0200

    create TransactionCard component

---
+++ components/TransactionCard/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function TransactionCard() {
+  return (
+    <li>
+      <p>name</p>
+      <p>amount</p>
+      <p>date</p>
+      <p>type</p>
+    </li>
+  );
+}
+
+export default TransactionCard;
