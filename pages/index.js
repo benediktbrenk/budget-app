44f3dd6b0ded5c548a267f2c2162aea93d942414
Haydar Algün <haydar.alguen@gmail.com>
2024-04-29 15:37:31 +0200

    add styled Link New Transaction

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,33 @@
 import Header from "@/components/Header";
 import TransactionList from "@/components/TransactionList";
 import Link from "next/link";
+import { styled } from "styled-components";
+
+const StyledDiv = styled.div`
+  padding: 1rem;
+`;
+
+const StyledLink = styled(Link)`
+  display: flex;
+  justify-content: center;
+  padding: 1.5rem;
+  text-decoration: none;
+  color: black;
+  background-color: grey;
+  margin: 1rem;
+  border-radius: 8px;
+  font-size: 1.1rem;
+`;
 
 export default function HomePage({ transactions }) {
   return (
-    <div>
+    <main>
       <Header title="Transactions" />
-      <Link href="/newentry">Add New Transaction</Link>
+      <StyledDiv>
+        <StyledLink href="/newentry">Add New Transaction</StyledLink>
+      </StyledDiv>
+
       <TransactionList transactions={transactions} />
-    </div>
+    </main>
   );
 }
