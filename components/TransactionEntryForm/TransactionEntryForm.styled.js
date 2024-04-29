03d36d5bdaa4e87e3a45d8bc2d7a75509074248b
Haydar Algün <haydar.alguen@gmail.com>
2024-04-29 15:04:06 +0200

    refactor code and styling

--- components/TransactionEntryForm/TransactionEntryForm.styled.js
+++ components/TransactionEntryForm/TransactionEntryForm.styled.js
@@ -8,11 +8,6 @@ export const StyledFormContainer = styled.div`
   gap: 0.75rem;
   min-width: 300px;
   max-width: 800px;
-
-  &select {
-    overflow-x: 100px;
-    overflow-y: 100px;
-  }
 `;
 
 export const StyledFormField = styled.div`
@@ -21,16 +16,6 @@ export const StyledFormField = styled.div`
   justify-content: space-between;
   //align-items: center;
   gap: 1rem;
-
-  label {
-    flex: 1;
-  }
-
-  select,
-  input,
-  textarea {
-    flex: 2;
-  }
 `;
 
 export const StyledFormButton = styled.div`
