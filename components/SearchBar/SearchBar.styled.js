abacaf1f7b246fd6202b341ec3db2abdcd42fb68
Haydar Algün <haydar.alguen@gmail.com>
2024-04-30 15:12:43 +0200

    update search function with date and Category

--- components/SearchBar/SearchBar.styled.js
+++ components/SearchBar/SearchBar.styled.js
@@ -46,3 +46,30 @@ export const StyledSearchInput = styled.input`
   padding: 1rem;
   margin-top: 5px;
 `;
+export const StyledSearchInputSelect = styled.select`
+  border-radius: 8px;
+  border: 1px solid black;
+  width: 100%;
+  height: 50px;
+  font-size: 1rem;
+  padding: 1rem;
+  margin-top: 5px;
+`;
+
+export const StyledFilterContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+  margin: 0.75rem;
+  padding: 0.5rem;
+  gap: 4rem;
+`;
+
+export const StyledFilterItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 0.5rem;
+`;
--- components/SearchBar/index.js
+++ components/SearchBar/index.js
@@ -1,32 +1,66 @@
 import TransactionCard from "../TransactionCard/index";
 import {
+  StyledFilterContainer,
+  StyledFilterItem,
   StyledSearchContainer,
   StyledSearchForm,
   StyledSearchInput,
+  StyledSearchInputSelect,
   StyledSearchUl,
 } from "./SearchBar.styled";
 
 function SearchBar({ transactions, search, onSearch }) {
+  const filterTransactions = () => {
+    return transactions.filter((transaction) => {
+      // Suche nach Name, Datum, Kategorie und Betrag
+      return (
+        transaction.name.toLowerCase().includes(search.toLowerCase()) ||
+        transaction.date.includes(search) ||
+        transaction.category.toLowerCase().includes(search.toLowerCase()) ||
+        transaction.amount.toString().includes(search)
+      );
+    });
+  };
+
   return (
     <StyledSearchContainer>
       <StyledSearchForm>
         <label>Search</label>
         <StyledSearchInput
           type="search"
-          placeholder="Search transactions..."
+          placeholder="Search Transactions..."
           onChange={(event) => onSearch(event.target.value)}
         />
+        <StyledFilterContainer>
+          <StyledFilterItem>
+            <label>Date</label>
+            <StyledSearchInput
+              type="date"
+              placeholder="Search by date..."
+              onChange={(event) => onSearch(event.target.value)}
+            />
+          </StyledFilterItem>
+          <StyledFilterItem>
+            <label>Category</label>
+            <StyledSearchInputSelect
+              onChange={(event) => onSearch(event.target.value)}
+            >
+              <option value="">All</option>
+              <option value="Groceries">Groceries</option>
+              <option value="Housing">Housing</option>
+              <option value="Insurance">Insurance</option>
+              <option value="Others">Others</option>
+            </StyledSearchInputSelect>
+          </StyledFilterItem>
+        </StyledFilterContainer>
       </StyledSearchForm>
       <StyledSearchUl>
-        {transactions.map((transaction) => {
-          if (transaction.name !== search) {
-            return;
-          }
-          if (transaction.name === search) {
-            return (
-              <TransactionCard key={transaction.id} transaction={transaction} />
-            );
-          }
+        {filterTransactions().map((transaction) => {
+          return search == "" ? (
+            <></>
+          ) : (
+            <TransactionCard key={transaction.id} transaction={transaction} />
+          );
         })}
       </StyledSearchUl>
     </StyledSearchContainer>
