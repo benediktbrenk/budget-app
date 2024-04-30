8b0eab92383b7169e7394503c1ea55dfc475601f
Marcel Herr <mrclhrr@gmail.com>
2024-04-30 14:55:24 +0200

    fixed router in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,14 @@ import { useState } from "react";
 import GlobalStyle from "../styles";
 import { transactions } from "@/db/data.js";
 import { uid } from "uid";
+import { useRouter } from "next/router";
 
 const initialTransactions = transactions;
 
 export default function App({ Component, pageProps }) {
   const [transactions, setTransactions] = useState(initialTransactions);
+
+  const router = useRouter();
 
   function handleAddTransaction(newTransaction) {
     setTransactions([{ id: uid(), ...newTransaction }, ...transactions]);
