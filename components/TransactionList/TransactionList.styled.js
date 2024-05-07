bf2d74e3f2a940a60794da47c48415573a1c435a
Benedikt Brenk <benedikt.brenk@outlook.de>
2024-05-07 14:23:18 +0200

    change calculation of transaction list width

--- components/TransactionList/TransactionList.styled.js
+++ components/TransactionList/TransactionList.styled.js
@@ -1,7 +1,13 @@
 import styled from "styled-components";
 
+const doubledBorder = "30px";
+const minimalDeviceWidth = "395px";
+
 export const Ul = styled.ul`
   display: flex;
   flex-direction: column;
-  width: min(100% - 30px, 395px - 30px);
+  width: min(
+    calc(100% - ${doubledBorder}),
+    calc(${minimalDeviceWidth} - ${doubledBorder})
+  );
 `;
