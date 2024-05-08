b47a45087776d5b3fe5842988c24d85cf7fa4c84
Benedikt Brenk <benedikt.brenk@outlook.de>
2024-05-08 16:31:14 +0200

    create function to sort transactions by month

--- components/TransactionList/index.js
+++ components/TransactionList/index.js
@@ -2,9 +2,19 @@ import TransactionCard from "../TransactionCard";
 import { Ul } from "./TransactionList.styled";
 
 function TransactionList({ transactions }) {
+  function filterTransactionsByMonth(transactions, currentMonth) {
+    const transactionsInMarch = transactions.filter((transaction) => {
+      const month = new Date(transaction.date).getMonth();
+      return month === currentMonth;
+    });
+    return transactionsInMarch;
+  }
+
+  const transactionsInMarch = filterTransactionsByMonth(transactions, 4);
+
   return (
     <Ul>
-      {transactions.map((transaction) => (
+      {transactionsInMarch.map((transaction) => (
         <TransactionCard key={transaction.id} transaction={transaction} />
       ))}
     </Ul>
