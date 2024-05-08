8d6d17294bc6bd03ac5bcb21b8f2c132f5ce0b1e
Haydar Algün <haydar.alguen@gmail.com>
2024-05-08 17:29:40 +0200

    created api for Post request in index.js

--- pages/api/transactions/index.js
+++ pages/api/transactions/index.js
@@ -8,6 +8,19 @@ export default async function handler(request, response) {
       const transactions = await Transaction.find();
       return response.status(200).json(transactions);
     }
+
+    if (request.method === "POST") {
+      try {
+        const transactionData = request.body;
+        const transaction = new Transaction(transactionData);
+        await transaction.save();
+        return response.status(201).json({ status: "Transaction added." });
+      } catch (error) {
+        console.error(error);
+        return response.status(400).json({ error: error.message });
+      }
+    }
+    
   } catch (error) {
     console.error("Error fetching transactions:", error.message);
     return response.status(400).json({ error: "Internal Server Error" });
