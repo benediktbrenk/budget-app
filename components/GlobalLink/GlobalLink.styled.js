dc3078a4c44f8e9b551f480b11ed2eedb69818bb
Marcel Herr <mrclhrr@gmail.com>
2024-05-13 11:45:09 +0200

    create linkl component

---
+++ components/GlobalLink/GlobalLink.styled.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+// export const globalLinkContainer = styled.div`
+//   min-height: 48px;
+//   min-width: 48px;
+// `;
---
+++ components/GlobalLink/index.js
@@ -0,0 +1,6 @@
+// import * as Styled from "./GlobalLink.styled";
+import Link from "next/link";
+
+export default function GlobalLink() {
+  return <Link href="/">hallo</Link>;
+}
