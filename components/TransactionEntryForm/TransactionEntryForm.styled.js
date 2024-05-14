7a83cc96102abfeafa67f3d3b6760cdfc4e1294d
Eric <Karstenswobi@gmail.com>
2024-05-14 16:31:56 +0200

    Styled FormField

--- components/TransactionEntryForm/TransactionEntryForm.styled.js
+++ components/TransactionEntryForm/TransactionEntryForm.styled.js
@@ -1,25 +1,51 @@
 import { styled } from "styled-components";
 
 export const FormContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
-  padding-top: 1rem;
-  gap: 0.75rem;
-  min-width: 300px;
-  max-width: 800px;
-  margin-bottom: 80px;
+display: flex;
+flex-direction: column;
+background-color: #f5f6f6;
+padding: 15px 35px 15px 35px;
+
+margin: 20px 0;
+border-radius: 0.75rem;
+min-width: 250px;
+max-width: 300px;
+box-shadow: 0.1rem 0.1rem 0.1rem 0.1rem rgba(0, 0, 0, 0.2);
+position: relative;
+
+&:before {
+  content: "";
+  position: absolute;
+  top: 2%;
+  left: 10px;
+  width: 17px;
+  height: 96%;
+  background-color: lightblue;
+  border-radius: 6px;
+}
 `;
+
+  
+
 
 export const FormField = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   justify-content: space-between;
-  gap: 1rem;
+  padding-left: 20px;
+  gap: 0.4rem;
+ 
 `;
 
 export const FormButton = styled.div`
   display: flex;
   justify-content: center;
   margin-top: 2rem;
+`;
+
+export const Button = styled.button`
+padding: 0.8rem 1.5rem;
+display: flex;
+
+justisfy-content: space-between;
 `;
--- components/TransactionEntryForm/index.js
+++ components/TransactionEntryForm/index.js
@@ -1,5 +1,6 @@
 import * as Styled from "./TransactionEntryForm.styled";
 import { useRouter } from "next/router";
+import React, { useState } from "react";
 
 function TransactionEntryForm({
   updateTransactions,
@@ -9,118 +10,150 @@ function TransactionEntryForm({
 }) {
   const router = useRouter();
 
-  function handleSubmit(event) {
+  const [showModal, setShowModal] = useState(false);
+
+  // Function to open modal
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  // Function to close modal
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  // Function to handle form submission
+  const handleSubmit = (event) => {
     event.preventDefault();
     const formData = new FormData(event.target);
     const data = Object.fromEntries(formData);
     const updatedTransaction = { ...data, amount: parseFloat(data.amount) };
     updateTransactions(updatedTransaction, id);
     event.target.reset();
+    setShowModal(false);
     router.push("/");
-  }
+  };
+
   return (
-    <form onSubmit={handleSubmit}>
-      <Styled.FormContainer>
-        <Styled.FormField>
-          <label htmlFor="direction">Direction</label>
-          <select
-            type="text"
-            name="direction"
-            required
-            defaultValue={
-              mode === "add" ? "Default" : currentTransaction.direction
-            }
-          >
-            <option value="Default">--Select Option--</option>
-            <option value="Expense">Expense</option>
-            <option value="Income">Income</option>
-          </select>
-        </Styled.FormField>
-        <Styled.FormField>
-          <label htmlFor="name">Title</label>
-          <input
-            type="text"
-            name="name"
-            maxLength={30}
-            required
-            defaultValue={mode === "add" ? "" : currentTransaction.name}
-          />
-        </Styled.FormField>
-        <Styled.FormField>
-          <label htmlFor="amount">Amount</label>
-          <input
-            type="number"
-            name="amount"
-            max="999999.99"
-            min="0.00"
-            step="0.01"
-            required
-            defaultValue={mode === "add" ? "" : currentTransaction.amount}
-          />
-        </Styled.FormField>
-        <Styled.FormField>
-          <label htmlFor="currency">Currency</label>
-          <select
-            type="text"
-            name="currency"
-            required
-            defaultValue={
-              mode === "add" ? "Default" : currentTransaction.currency
-            }
-          >
-            <option value="Default">--Select Option--</option>
-            <option value="€">€</option>
-          </select>
-        </Styled.FormField>
-        <Styled.FormField>
-          <label htmlFor="date">Date</label>
-          <input
-            type="date"
-            name="date"
-            required
-            defaultValue={mode === "add" ? "" : currentTransaction.date}
-          />
-        </Styled.FormField>
-        <Styled.FormField>
-          <label htmlFor="paymentMethod">Payment Method</label>
-          <select
-            type="text"
-            name="paymentMethod"
-            required
-            defaultValue={
-              mode === "add" ? "" : currentTransaction.paymentMethod
-            }
-          >
-            <option value="Default">--Select Option--</option>
-            <option value="Cash">Cash</option>
-            <option value="Card">Card</option>
-          </select>
-        </Styled.FormField>
-        <Styled.FormField>
-          <label htmlFor="category">Category</label>
-          <select type="text" name="category" required defaultValue="Groceries">
-            <option value="Groceries">Groceries</option>
-            <option value="Salary">Salary</option>
-            <option value="Housing">Housing</option>
-            <option value="Insurance">Insurance</option>
-            <option value="Utilities">Utilities</option>
-          </select>
-        </Styled.FormField>
-        <Styled.FormField>
-          <label htmlFor="description">Description</label>
-          <textarea
-            rows="7"
-            maxLength={140}
-            type="text"
-            name="description"
-            defaultValue={mode === "add" ? "" : currentTransaction.description}
-          />
-        </Styled.FormField>
-        <Styled.FormButton>
-          <button type="submit">Submit</button>
-        </Styled.FormButton>
-      </Styled.FormContainer>
-    </form>
+    <>
+      <button onClick={openModal}>Edit</button>
+      {showModal && (
+        <div className="modal">
+          <form onSubmit={handleSubmit}>
+            <Styled.FormContainer>
+              <Styled.FormField>
+                <label htmlFor="direction">Direction</label>
+                <select
+                  type="text"
+                  name="direction"
+                  required
+                  defaultValue={
+                    mode === "add" ? "Default" : currentTransaction.direction
+                  }
+                >
+                  <option value="Default">--Select Option--</option>
+                  <option value="Expense">Expense</option>
+                  <option value="Income">Income</option>
+                </select>
+              </Styled.FormField>
+              <Styled.FormField>
+                <label htmlFor="name">Title</label>
+                <input
+                  type="text"
+                  name="name"
+                  maxLength={30}
+                  required
+                  defaultValue={mode === "add" ? "" : currentTransaction.name}
+                />
+              </Styled.FormField>
+              <Styled.FormField>
+                <label htmlFor="amount">Amount</label>
+                <input
+                  type="number"
+                  name="amount"
+                  max="999999.99"
+                  min="0.00"
+                  step="0.01"
+                  required
+                  defaultValue={
+                    mode === "add" ? "" : currentTransaction.amount
+                  }
+                />
+              </Styled.FormField>
+              <Styled.FormField>
+                <label htmlFor="currency">Currency</label>
+                <select
+                  type="text"
+                  name="currency"
+                  required
+                  defaultValue={
+                    mode === "add" ? "Default" : currentTransaction.currency
+                  }
+                >
+                  <option value="Default">--Select Option--</option>
+                  <option value="€">€</option>
+                </select>
+              </Styled.FormField>
+              <Styled.FormField>
+                <label htmlFor="date">Date</label>
+                <input
+                  type="date"
+                  name="date"
+                  required
+                  defaultValue={
+                    mode === "add" ? "" : currentTransaction.date
+                  }
+                />
+              </Styled.FormField>
+              <Styled.FormField>
+                <label htmlFor="paymentMethod">Payment Method</label>
+                <select
+                  type="text"
+                  name="paymentMethod"
+                  required
+                  defaultValue={
+                    mode === "add" ? "" : currentTransaction.paymentMethod
+                  }
+                >
+                  <option value="Default">--Select Option--</option>
+                  <option value="Cash">Cash</option>
+                  <option value="Card">Card</option>
+                </select>
+              </Styled.FormField>
+              <Styled.FormField>
+                <label htmlFor="category">Category</label>
+                <select
+                  type="text"
+                  name="category"
+                  required
+                  defaultValue={mode === "add" ? "Groceries" : currentTransaction.category}
+                >
+                  <option value="Groceries">Groceries</option>
+                  <option value="Salary">Salary</option>
+                  <option value="Housing">Housing</option>
+                  <option value="Insurance">Insurance</option>
+                  <option value="Utilities">Utilities</option>
+                </select>
+              </Styled.FormField>
+              <Styled.FormField>
+                <label htmlFor="description">Description</label>
+                <textarea
+                  rows="7"
+                  maxLength={140}
+                  type="text"
+                  name="description"
+                  defaultValue={mode === "add" ? "" : currentTransaction.description}
+                />
+              </Styled.FormField>
+            </Styled.FormContainer>
+            
+              <Styled.Button type="submit">Submit</Styled.Button>
+              <Styled.Button type="button" onClick={closeModal}>Cancel</Styled.Button>
+            
+          </form>
+        </div>
+      )}
+    </>
   );
 }
 
