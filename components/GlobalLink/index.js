af0c8ab8d86dadec3fe591bea055cfa4408e94fe
Marcel Herr <mrclhrr@gmail.com>
2024-05-14 14:53:40 +0200

    delete button and link component

--- components/GlobalLink/index.js
+++
@@ -1,9 +0,0 @@
-import * as Styled from "./GlobalLink.styled";
-
-export default function GlobalLink({ href, name, type, textColor }) {
-  return (
-    <Styled.LinkButton href={href} type={type} textColor={textColor}>
-      {name}
-    </Styled.LinkButton>
-  );
-}
