91dd0bd4dc37232d277a34cf3113c15c50a99df0
Benedikt Brenk <benedikt.brenk@outlook.de>
2024-05-14 08:54:36 +0200

    create action icon styled components

--- components/TransactionCard/TransactionCard.styled.js
+++ components/TransactionCard/TransactionCard.styled.js
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 import Link from "next/link";
+import { TiEdit } from "react-icons/ti";
+import { IoInformationCircleOutline } from "react-icons/io5";
+import { FaRegTrashCan } from "react-icons/fa6";
 
 export const CardLink = styled(Link)`
   text-decoration: none;
@@ -54,6 +57,50 @@ export const TransactionDate = styled.p`
   font: 400 12px/14px var(--font-family);
 `;
 
+export const ActionLinkContainer = styled.div`
+  flex: 3;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const ActionLink = styled(Link)`
+  text-decoration: none;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  height: 30px;
+  width: 30px;
+  color: black;
+`;
+
+export const ActionButton = styled.button`
+  text-decoration: none;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  height: 30px;
+  width: 30px;
+  border: none;
+  background-color: transparent;
+  padding: 0;
+`;
+
+export const ActionInfo = styled(IoInformationCircleOutline)`
+  height: 26px;
+  width: 26px;
+`;
+
+export const ActionDetails = styled(TiEdit)`
+  height: 25px;
+  width: 25px;
+`;
+
+export const ActionDelete = styled(FaRegTrashCan)`
+  height: 21px;
+  width: 21px;
+`;
+
 export const TransactionAmount = styled.p`
   flex: 1;
   color: ${(props) => {
@@ -62,6 +109,6 @@ export const TransactionAmount = styled.p`
       : "var(--amount-color-red)";
   }};
   margin: 0;
-  font: 600 14px/16px var(--font-family);
+  font: 600 16px/18px var(--font-family);
   text-align: right;
 `;
