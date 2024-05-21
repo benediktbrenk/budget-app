d0e39a71673b3fb5da3bba4eaa557fcd19047232
Marcel Herr <mrclhrr@gmail.com>
2024-05-21 10:15:06 +0200

    Relocate title and update icon size

--- components/Header/Header.styled.js
+++ components/Header/Header.styled.js
@@ -2,16 +2,14 @@ import styled from "styled-components";
 
 export const HeaderContainer = styled.header`
   display: flex;
-  justify-content: center;
-  align-items: center;
   width: 100%;
   height: 70px;
-  margin-top: 10px;
+  margin-top: 5px;
   margin-bottom: 15px;
   display: flex;
   justify-content: space-evenly;
   align-items: center;
-  padding: 25px;
+  padding: 20px;
   gap: 20px;
 `;
 
@@ -19,4 +17,6 @@ export const Headline = styled.h1`
   color: var(--header-textColor);
   font-size: 1.6rem;
   text-align: center;
+  margin-left: -15px;
+  padding-right: 50px;
 `;
