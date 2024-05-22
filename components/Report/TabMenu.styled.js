e0e0fb5a41fe6a8f1236234ce86f34e798bd1801
Marcel Herr <mrclhrr@gmail.com>
2024-05-22 14:46:37 +0200

    q

--- components/Report/TabMenu.styled.js
+++ components/Report/TabMenu.styled.js
@@ -30,10 +30,4 @@ export const TabContent = styled.div`
   font-family: Arial, sans-serif;
   margin-bottom: 80px;
   color: black;
-  input,
-  select,
-  textarea {
-    color: black;
-    -webkit-text-fill-color: black;
-  }
 `;
