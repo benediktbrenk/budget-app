3d2f2adf8ef4a5fbec5ad3afaa11ba9780340ba5
Haydar Algün <haydar.alguen@gmail.com>
2024-05-23 12:51:56 +0200

    change styling in profilepage

--- components/ProfilePage/index.js
+++ components/ProfilePage/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import * as Styled from "./Profile.styled";
-import { ProfileIconWrapper } from "../LoginButton/LoginButton.styled";
 import { FaCircleUser } from "react-icons/fa6";
 import { useSession } from "next-auth/react";
 import ProtectPage from "../ProtectPages";
+import Image from "next/image";
 
 const Profile = () => {
   const { data: session, status } = useSession();
@@ -15,24 +15,37 @@ const Profile = () => {
   if (!session) {
     return <div>No session available. Please log in.</div>;
   }
+  console.log(session);
 
   return (
     <ProtectPage>
-      <Styled.Container>
-        <h1>Profile</h1>
-        <Styled.ProfileIconWrapper>
-          <FaCircleUser size={50} />
-        </Styled.ProfileIconWrapper>
-
-        <div>
-          <h3>Name:</h3>
-          <p>{session.user.name}</p>
-        </div>
-        <div>
-          <h3>Email:</h3>
-          <p>{session.user.email}</p>
-        </div>
-      </Styled.Container>
+      <Styled.ContainerBox>
+        <Styled.ContainerCard>
+          <h1>Profile</h1>
+          <Styled.ProfileIconWrapper>
+            {session.user.image ? (
+              <Image
+                src={session.user.image}
+                width={160}
+                height={160}
+                alt="user image"
+              />
+            ) : (
+              <FaCircleUser fontSize={180} />
+            )}
+          </Styled.ProfileIconWrapper>
+          <Styled.ProfileContent>
+            <Styled.ProfileContentItem>
+              <h3>Name:</h3>
+              <p>{session.user.name}</p>
+            </Styled.ProfileContentItem>
+            <Styled.ProfileContentItem>
+              <h3>Email:</h3>
+              <p>{session.user.email ? session.user.email : "Secret"}</p>
+            </Styled.ProfileContentItem>
+          </Styled.ProfileContent>
+        </Styled.ContainerCard>
+      </Styled.ContainerBox>
     </ProtectPage>
   );
 };
