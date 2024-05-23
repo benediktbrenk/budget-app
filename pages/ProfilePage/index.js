2c5aaf4ab41bb2d611b2148274d4b4d64fa196d9
Haydar Algün <haydar.alguen@gmail.com>
2024-05-23 14:55:12 +0200

    edit profile page

--- pages/ProfilePage/index.js
+++ pages/ProfilePage/index.js
@@ -1,18 +1,10 @@
 import Profile from "@/components/ProfilePage";
 import React from "react";
 
-const user = {
-  name: "John Doe",
-  email: "john@example.com",
-  age: 30,
-};
-
-const ProfilePage = () => {
+export default function ProfilePage() {
   return (
-    <div>
-      <Profile user={user} />
-    </div>
+    <>
+      <Profile />
+    </>
   );
-};
-
-export default ProfilePage;
+}
